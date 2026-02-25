acfb1f97cdb52eeac621a3f3ca861b3b96ec79c5
carrotwaxr <carrotwaxr@users.noreply.github.com>
2026-02-25 14:14:43 -0800

    chore: bump version to 0.1.2-beta.15

--- api/settings_router.py
+++ api/settings_router.py
@@ -20,7 +20,7 @@ router = APIRouter(tags=["settings"])
 
 # Set at startup
 _start_time: Optional[float] = None
-_version: str = "0.1.2-beta.14"
+_version: str = "0.1.2-beta.15"
 
 
 def init_settings_router():
